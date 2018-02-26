63cc12341c5b07c31af50a6ff26948f4b8686f23
Jim Smart <jim@jimsmart.org>
2018-02-26 16:51:51 +0000

    Cleanup

--- dialect.go
+++ dialect.go
@@ -22,18 +22,18 @@ type dialect struct {
 // This is somewhat fragile.
 var driverDialect map[string]*dialect = map[string]*dialect{
 	"*sqlite3.SQLiteDriver":       &sqlite,   // github.com/mattn/go-sqlite3
-	"*sqlite.impl":                &sqlite,   // TODO(js) UNTESTED github.com/gwenn/gosqlite
-	"sqlite3.Driver":              &sqlite,   // TODO(js) UNTESTED github.com/mxk/go-sqlite
+	"*sqlite.impl":                &sqlite,   // github.com/gwenn/gosqlite
+	"sqlite3.Driver":              &sqlite,   // github.com/mxk/go-sqlite - TODO(js) No datatypes.
 	"*pq.Driver":                  &postgres, // github.com/lib/pq
-	"*stdlib.Driver":              &postgres, // TODO(js) UNTESTED github.com/jackc/pgx/stdlib
-	"*pgsqldriver.postgresDriver": &postgres, // TODO(js) UNTESTED github.com/jbarham/gopgsqldriver
+	"*stdlib.Driver":              &postgres, // github.com/jackc/pgx
+	"*pgsqldriver.postgresDriver": &postgres, // github.com/jbarham/gopgsqldriver - TODO(js) No datatypes.
 	"*mysql.MySQLDriver":          &mysql,    // github.com/go-sql-driver/mysql
-	"*godrv.Driver":               &mysql,    // TODO(js) UNTESTED github.com/ziutek/mymysql
+	"*godrv.Driver":               &mysql,    // github.com/ziutek/mymysql - TODO(js) No datatypes.
 	"*mssql.MssqlDriver":          &mssql,    // github.com/denisenkom/go-mssqldb
-	"*freetds.MssqlDriver":        &mssql,    // TODO(js) UNTESTED github.com/minus5/gofreetds
+	"*freetds.MssqlDriver":        &mssql,    // github.com/minus5/gofreetds - TODO(js) No datatypes. Error on create view.
 	"*goracle.drv":                &oracle,   // gopkg.in/goracle.v2
-	"*ora.Drv":                    &oracle,   // TODO(js) UNTESTED gopkg.in/rana/ora.v4
-	"*oci8.OCI8Driver":            &oracle,   // TODO(js) UNTESTED github.com/mattn/go-oci8
+	"*ora.Drv":                    &oracle,   // gopkg.in/rana/ora.v4 - TODO(js) Mismatched datatypes.
+	"*oci8.OCI8Driver":            &oracle,   // github.com/mattn/go-oci8 - TODO(js) Mismatched datatypes.
 }
 
 // TODO Should we expose a method of registering a driver string/dialect in our registry?
