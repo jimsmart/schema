9017459f485df02845185e5c0c0b73a58a6ed29a
Jim Smart <jim@jimsmart.org>
2019-11-08 01:47:36 +0000

    Cleanup of MSSQL init

--- docker-db-init-mssql.sh
+++ docker-db-init-mssql.sh
@@ -1,9 +1,18 @@
 # Based upon comments here https://github.com/Microsoft/mssql-docker/issues/11
 
-# TODO(js) This sleep is terrible, we need to find a better solution
-# (I think it will cause problems on Travis)
-# We should instead do something like https://github.com/Microsoft/mssql-docker/issues/11
-sleep 30s
+# Wait for MSSQL to start.
+while [ true ]; do
+    sleep 1s
+    /opt/mssql-tools/bin/sqlcmd -l 30 -S localhost -h-1 -V1 -U sa -P "$SA_PASSWORD" -Q "select name from sys.databases where state_desc != 'ONLINE'" | grep --quiet '0 rows affected' > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        # All databases are online.
+        break
+    fi
+    # Retry.
+done
 
-echo "running MSSQL setup script"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -i init.sql
+echo "SQL Server is up. Running init.sql script."
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -i init.sql
+
+# TODO(js) Can we check whether this has already been run enforce 'only run once'?
+# Or should that be the responsibility of the .sql script? Or?
