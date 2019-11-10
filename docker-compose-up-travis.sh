70f0eded0e557a4f74a35da2af892bad4dc977f8
Jim Smart <jim@jimsmart.org>
2019-11-10 23:58:07 +0000

    Travis: script cleanups

--- docker-compose-up-travis.sh
+++ docker-compose-up-travis.sh
@@ -1,4 +1,10 @@
 if [ "$0" != "sqlite" ]; then
     exit 0;
 fi
+if [ "$0" == "oracle" ]; then
+    echo "Performing Docker login for Oracle Container Registry"
+    echo "$OCR_PASSWORD" | docker login -u "$OCR_USERNAME" --password-stdin container-registry.oracle.com
+fi
+
+echo "Using Docker Compose to bring up service: $0"
 docker-compose up -d $0
